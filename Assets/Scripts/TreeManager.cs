e6cff624b96f6d22e0c59e8b3750fb8709a73a63
Jerom <jerom.utrecht@gmail.com>
2016-08-19 11:28:53 +0200

    Added XML loading and saving capability.
    Added XML loading and saving capability for the technology trees.

---
+++ Assets/Scripts/TreeManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeManager : MonoBehaviour {
+
+    [UnityEngine.SerializeField]
+    private ItemDatabase itemDB;
+
+    [UnityEngine.SerializeField]
+    private string path = "/Technology/Software Tech Tree.xml";
+
+    public void Load()
+    {
+        itemDB = XMLManager.instance.Load(path);
+    }
+
+    public void Save()
+    {
+        XMLManager.instance.Save(itemDB, path);
+    }
+}
